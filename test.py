e28b23ba921cd12b68ecab714427e0be56f45b64
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2021-12-15 10:10:47 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+"""
+A test script to check that sequences without comments
+are properly managed by pyfastx.
+A test file with a sequence named "nocomment" that must
+be without comments, preceded by at least one sequence with
+a comment, should trigger the behaviour.
+"""
 import pyfastx
 import sys
 input = sys.argv[1] if len(sys.argv) > 1 else "test.fa"
